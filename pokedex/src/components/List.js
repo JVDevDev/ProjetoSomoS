07efedd34a2f62cd5c7df6f17ef5e975810c6f34
Cloud <jvhc3002@gmail.com>
2022-08-11 22:35:03 -0300

    teste unitário api

--- pokedex/src/components/List.js
+++ pokedex/src/components/List.js
@@ -1,11 +1,14 @@
 import './List.css';
 import ControlPage from './ControlPage'
+import Api from './Api'
 
 
 function List (){
   return(
     <div className='list'>
       <p className='logo'>Pokemons</p>
+
+      <Api />
       
       <ControlPage />
     </div>
