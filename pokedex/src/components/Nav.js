35df2270ef31073fd44fdc50530930d7382d3910
Cloud <jvhc3002@gmail.com>
2022-08-14 15:07:26 -0300

    Feature: Implementação da api na tela

--- pokedex/src/components/Nav.js
+++ pokedex/src/components/Nav.js
@@ -1,34 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import Api from '../services/Api'
+import usePages from '../hooks/usePages'
+import useAxios from '../hooks/useAxios'
+
 import '../styles/Nav.css'
 
 function Nav() {
-    // eslint-disable-next-line
-    const [page, setPage] = useState(10)
-    const [namePokemon, setNamePokemon] = useState([]);
+  
+    
+    const { page } = usePages;
+    
+    const { pokemon } = useAxios(`?limit=6&offset=${page}`)
 
-    useEffect(() => {
-        Api.get(`?limit=5&offset=${page}`)
-            .then(res => {
-                const dados = res.data.results
-                setNamePokemon(dados.map((nomes)=>{
-                  return nomes.name
-                }))
-            })
-            .catch(erro => {
-                console.log('Deu erro aqui, calma: ' + erro)
-            })
-        // eslint-disable-next-line
-    }, [page])
+    
+    
 
-    console.log({namePokemon})
     return (
         <div id="lista" className="nav">
-            <button className="itemList">pokemon 1</button>
-            <button className="itemList">pokemon 2</button>
-            <button className="itemList">pokemon 3</button>
-            <button className="itemList">pokemon 4</button>
-            <button className="itemList">pokemon 5</button>
+            {pokemon.map((nomes) => 
+            <button className='itemList'>{nomes.name}</button>
+            )}
         </div>
     )
 }
