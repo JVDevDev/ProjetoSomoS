9da0200c6b88d6d1bba187ee7356c294b35888db
Cloud <jvhc3002@gmail.com>
2022-08-14 03:26:37 -0300

    Estudo da api

--- pokedex/src/components/Nav.js
+++ pokedex/src/components/Nav.js
@@ -1,40 +1,32 @@
-import React, { useEffect, useState } from "react";
-import Api from '../services/Api';
-import '../styles/Nav.css';
+import React, { useEffect, useState } from 'react'
+import Api from '../services/Api'
+import '../styles/Nav.css'
 
-function Nav (){
-  // eslint-disable-next-line
-  const [page, setPage] = useState(0);
-  const [name, setName] = useState([]);
+function Nav() {
+    // eslint-disable-next-line
+    const [page, setPage] = useState(10)
+    
 
+    useEffect(() => {
+        Api.get(`?limit=5&offset=${page}`)
+            .then(res => {
+                console.log(res.data.results)
+            })
+            .catch(erro => {
+                console.log('Deu erro aqui, calma: ' + erro)
+            })
+        // eslint-disable-next-line
+    }, [page])
 
-  useEffect(() => {
-    const pokemons = []
-    Api
-      .get(`?limit=10&offset=${page}`)
-      .then((res) => {
-        pokemons.push(res.data.results)
-        pokemons.forEach((mostrar) => {
-          mostrar.forEach((nome) =>{
-            setName(nome.name)
-            console.log({name})
-            
-          })
-        });
-      })
-      .catch((erro) => {
-        console.log("Deu erro aqui calma: " + erro)
-      })
-      // eslint-disable-next-line
-  }, [page])
-  
-  
-  return(
-    <div id="lista" className='nav'>
-      <li className="itemList">Bubasaulr</li>
-      <li className="itemList">Arroz</li>
-    </div>
-  );
+    return (
+        <div id="lista" className="nav">
+            <button className="itemList">pokemon 1</button>
+            <button className="itemList">pokemon 2</button>
+            <button className="itemList">pokemon 3</button>
+            <button className="itemList">pokemon 4</button>
+            <button className="itemList">pokemon 5</button>
+        </div>
+    )
 }
 
-export default Nav;
+export default Nav
